da5bb239b93cbf2e0e18c68a4b765d8e99911a6c
muahahack <muahahack@ukr.net>
2020-12-16 18:49:39 +0200

    add2

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
+});
+
+$('.js-product-link').click(function() {
+  $('.products__buy-wrapper').removeClass('js-hidden');
+});
+
+function calc() {
+  var size = document.getElementById("size");
+  var color = document.getElementById("color");
+  var stand = document.getElementById("stand");
+  var result = document.getElementById("result"); 
+
+  var price = 0;
+  price += parseInt(size.options[size.selectedIndex].value);
+  price += parseInt(color.options[color.selectedIndex].value);
+  price += parseInt(stand.options[stand.selectedIndex].value);
+
+  result.innerHTML = price;
+}
